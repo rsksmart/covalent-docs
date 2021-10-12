e96f4cf89d5571664a1ddb4462d784c44fa3599e
chenhua <chen@covalenthq.com>
2021-10-12 09:35:48 -0700

    fixed typos in contains operators in SQL-generator.js, LookML-generator.js, and LookMLAllInOne-generator.js

--- docs/components/SQL-generator.js
+++ docs/components/SQL-generator.js
@@ -94,7 +94,7 @@ SELECT <br/>
 {"    " + dfields.join(", \n    ")} <br />
 FROM {this.state.chain_name}.block_log_events e <br />
 WHERE <br />
-  {"    "} e.topics @> ARRAY[{"'\\x" + s.substr(2) + "'"}::byta]  <br/>
+  {"    "} e.topics @> ARRAY[{"'\\x" + s.substr(2) + "'"}::bytea]  <br/>
   {"    "} AND e.topics[1] = {"'\\x" + s.substr(2) + "'"}  <br/>
   {"    "} AND e.sender = {"'\\x" + this.state.address_input.substr(2) + "'"} <br />
 </pre>;
